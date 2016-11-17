1a55a506971dbc032824284abc9ccfc995af23b9
mikeyu152 <myu@blendlabs.com>
2016-11-17 08:18:51 -0800

    added graph traits

--- index.py
+++ index.py
@@ -2,35 +2,15 @@ from snap import *
 
 graph = LoadEdgeList(PNGraph, "celegans_n306.txt", 0, 1)
 
-def simulateSIR(graph, mu, beta, start):
-	numNodes = graph.GetNodes()
-	nodeArray = [node.GetId() for node in graph.Nodes()]
-	allNodes = Set(nodeArray)
-	infected = Set([nodeArray[start]])
-	susceptible = allNodes.difference(infected)
-	recovered = Set()
-	while len(infected) > 0:
-		for i in range(1, 1+numNodes):
-			if i in susceptible and i not in infected:
-				node = graph.GetNI(i)
-				deg = node.GetDeg()
-				for j in range(deg):
-					neighbor = node.GetNbrNId(j)
-					if neighbor in infected and random.random() < beta:
-						susceptible.remove(i)
-						infected.add(i)
-						break
-			elif i in infected:
-				if random.random() < mu:
-					infected.remove(i)
-					recovered.add(i)
-	return (len(susceptible), len(recovered))
+print 'nodes', graph.GetNodes()
 
-def processGraph(filename, start):
-	graph = LoadEdgeList(PUNGraph, filename, 0, 1)
-	numNodes = graph.GetNodes()
-	numSus, numRecovered = simulateSIR(graph, .5, .05, start)
-	if numSus + numRecovered != numNodes:
-		print "WHAT THE FUCK"
-	return numRecovered * 1.0 / (numSus + numRecovered)
+print 'average degree', graph.GetEdges()*1.0/graph.GetNodes()
 
+print "clustering coefficient", GetClustCf(graph)
+
+def getDegreeDist(graph):
+	degrees = TIntPrV()
+	GetOutDegCnt(graph, degrees)
+	return [(item.GetVal1(), item.GetVal2()) for item in degrees]
+
+print 'degree distribution', getDegreeDist(graph)
